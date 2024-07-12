df0ac599a3d1f2c31b9fbe2ced8b7eccdc6d616f
Герман Андреев <hellicformone2015@yandex.ru>
2024-07-13 00:32:42 +0300

    finish

--- ARCANOID/main.c
+++ ARCANOID/main.c
@@ -5,17 +5,27 @@
 #include <string.h>
 #include <conio.h>
 #include <Windows.h>
+#include <math.h>
 
 #define width 65
 #define height 25
+#define M_PI 3.14159265358979323846
 
 typedef struct {
 	int x, y;
 	int w;
 } TRacket;
 
+typedef struct {
+	float x, y;
+	int ix, iy;
+	float alfa;
+	float speed;
+} TBall;
+
 char mas[height][width + 1];
 TRacket racket;
+TBall ball;
 
 // инициализируем ракетку по середине карты
 void InitRacket() {
@@ -43,6 +53,67 @@ void MoveRacket(int x) {
 		racket.x = width - 1 - racket.w;
 	}
 }
+
+void MoveBall(float x, float y) {
+	ball.x = x;
+	ball.y = y;
+	ball.ix = (int)round(ball.x);
+	ball.iy = (int)round(ball.y);
+}
+
+void InitBall() {
+	MoveBall(2, 2);
+	ball.alfa = -1;
+	ball.speed = 0.5;
+}
+
+void PutBall() {
+	mas[ball.iy][ball.ix] = '*';
+}
+
+void AutoMoveBall() {
+	// делаем альфу нормальной
+	if (ball.alfa < 0) ball.alfa += M_PI * 2;
+	if (ball.alfa > M_PI * 2) ball.alfa -= M_PI * 2;
+
+	// перед перемещением сохраним значение нашего шарика
+	TBall bl = ball;
+
+	// выполняем перемещение до тех пор, пока не будет достигнута новая позиция
+	while (1) {
+		MoveBall(ball.x + cos(ball.alfa) * ball.speed, ball.y + sin(ball.alfa) * ball.speed);
+
+		// проверяем столкнулся ли он со стеной или с ракеткой
+		if (mas[ball.iy][ball.ix] == '#' || mas[ball.iy][ball.ix] == '@') {
+			// сравнием новое положение шарика и старое, bl старое положение шарика
+			if ((ball.ix != bl.ix) && (ball.iy != bl.iy)) {
+				// если старый игрик и новый икс шарика равны новому игрику и старому иксу
+				if (mas[bl.iy][ball.ix] == mas[ball.iy][bl.ix]) {
+					ball.alfa = bl.alfa + M_PI;
+				}
+				else {
+					if (mas[bl.iy][ball.ix] == '#')
+						ball.alfa = (2 * M_PI - bl.alfa) + M_PI;
+					else
+						ball.alfa = (2 * M_PI - bl.alfa);
+				}
+			}
+			else if (ball.iy == bl.iy) {
+				ball.alfa = (2 * M_PI - bl.alfa) + M_PI;
+			}
+			else {
+				ball.alfa = (2 * M_PI - bl.alfa);
+			}
+
+			// сохраняем новое значение угла
+			bl = ball;
+		}
+		else {
+			break;
+		}
+	}
+}
+
 
 void init() {
 	// 0-ую строку заполнили решетками
@@ -82,15 +153,27 @@ void setcur(int x, int y) {
 }
 
 int main() {
-	// переменная куда будем сохранять нажатый символ
+	BOOL run = FALSE;
 	InitRacket();
+	InitBall();
 	do {
 		setcur(0, 0);
+		if (run) {
+			AutoMoveBall();
+		}
+		if (ball.iy > height) {
+			run = FALSE;
+		}
 		init();
 		PutRacket();
+		PutBall();
 		show();
 		if (GetKeyState('A') < 0) MoveRacket(racket.x - 1);
 		if (GetKeyState('D') < 0) MoveRacket(racket.x + 1);
+		if (GetKeyState('W') < 0) run = TRUE;
+		if (!run) {
+			MoveBall(racket.x + racket.w / 2, racket.y - 1);
+		}
 		Sleep(10);
 	} 
 	while (GetKeyState(VK_ESCAPE) >= 0);
